81c85e773db29e565e3d8a0feb90d891f14e6e31
Raf Fonseca <raf.fonseca@shopify.com>
2025-10-03 14:00:38 -0400

    setup for training

--- main.py
+++ main.py
@@ -50,3 +50,62 @@ def text_from_ids(ids):
     return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
 
 
+# TODO 2 convert the text vector into a stream of character indices.
+all_ids = ids_from_chars(tf.strings.unicode_split(text, "UTF-8"))
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+
+# print the first 10 ids
+# for ids in ids_dataset.take(10):
+#     print(chars_from_ids(ids).numpy().decode("utf-8"))
+
+# create a stream of character indices.
+seq_length = 100
+examples_per_epoch = len(text) // (seq_length + 1)
+
+# batch converts chars into sequences of ids
+sequences = ids_dataset.batch(seq_length + 1, drop_remainder=True)
+
+# for seq in sequences.take(1):
+#     print(chars_from_ids(seq))
+
+# for seq in sequences.take(5):
+#     print(text_from_ids(seq).numpy())
+
+
+def split_input_target(sequence):
+    input_text = sequence[:-1]
+    target_text = sequence[1:]
+    return input_text, target_text
+
+split_input_target(list("Tensorflow"))
+
+dataset = sequences.map(split_input_target)
+
+# for input_example, target_example in dataset.take(1):
+#     print("Input :", text_from_ids(input_example).numpy())
+#     print("Target:", text_from_ids(target_example).numpy())
+
+
+# Batch size
+BATCH_SIZE = 64
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences,
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# it maintains a buffer in which it shuffles elements).
+BUFFER_SIZE = 10000
+
+dataset = (
+    dataset.shuffle(BUFFER_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True)
+    .prefetch(tf.data.experimental.AUTOTUNE)
+)
+
+# Length of the vocabulary in chars
+vocab_size = len(vocab)
+
+# The embedding dimension
+embedding_dim = 256
+
+# Number of RNN units
+rnn_units = 1024
