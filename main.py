30655555fc9179d100df7eee612e6c6db938d636
Raf Fonseca <raf.fonseca@shopify.com>
2025-10-03 14:10:28 -0400

    MyModel class with embedding, GRU, and dense layers

--- main.py
+++ main.py
@@ -107,5 +107,46 @@ vocab_size = len(vocab)
 # The embedding dimension
 embedding_dim = 256
 
-# Number of RNN units
-rnn_units = 1024
+# Number of RNN units (number of neurons in the hidden layer)
+rnn_units = 1024 
+
+
+# Build the model with these layers:
+# - Embedding layer maps each charater id into a vector 
+# - GRU layer type of RNN
+# - Dense layer output layer
+
+class MyModel(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, rnn_units):
+        super().__init__(self)
+        # TODO - Create an embedding layer
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        # TODO - Create a GRU layer
+        self.gru = tf.keras.layers.GRU(
+            rnn_units, return_sequences=True, return_state=True
+        )
+        # TODO - Finally connect it with a dense layer
+        self.dense = tf.keras.layers.Dense(vocab_size)
+
+    
+    def call(self, inputs, states=None, return_state=False, training=False):
+        x = self.embedding(inputs, training=training)
+        # since we are training a text generation model,
+        # we use the previous state, in training. If there is no state,
+        # then we initialize the state
+        if states is None:
+            states = self.gru.get_initial_state(x)
+        x, states = self.gru(x, initial_state=states, training=training)
+        x = self.dense(x, training=training)
+
+        if return_state:
+            return x, states
+        else:
+            return x
+        
+model = MyModel(
+    # Be sure the vocabulary size matches the `StringLookup` layers.
+    vocab_size=len(ids_from_chars.get_vocabulary()),
+    embedding_dim=embedding_dim,
+    rnn_units=rnn_units,
+)
